209ac272bbde401960c119ef4763d0d3abbf9644
whitejeannie20-dotcom <whitejeannie20@gmail.com>
2026-06-23 20:09:18 -0400

    Remove Practice.java with vehicle classes and methods done

--- ch4_operations/Practice.java
+++ ch4_operations/Practice.java
@@ -1,19 +1,1 @@
 
-class Practice {
-	class Suburu extends Vehicle {}
-	class Vehicle {}
-	public static void instance() {
-		Suburu s = new Suburu();
-		Vehicle v = new Vehicle();
-		
-		System.out.println(s instanceof Vehicle);
-	}
-	
-	public static void main(String[] args) {
-		int x = 5;
-		x *= 4 + 5;
-		System.out.println(5.0 == 5d);
-		
-		instance();
-	}
-}
